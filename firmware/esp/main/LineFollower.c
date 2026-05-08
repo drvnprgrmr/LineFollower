f1d4739c8f1145f1baff1a3b607cd9a3bceb355b
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2026-05-08 22:55:15 +0100

    replace previous provisioner with another and setup basic config

--- firmware/esp/main/LineFollower.c
+++ firmware/esp/main/LineFollower.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "wifi_provisioner.h"
+
+#define APP_NAME "LineFollower"
 
 void app_main(void)
 {
+    // Configure and start the provisioner
+    wifi_prov_config_t config = WIFI_PROV_DEFAULT_CONFIG();
+    config.ap_ssid = APP_NAME;
 
+    ESP_ERROR_CHECK(wifi_prov_start(&config));
+
+    // Block until connected (or use the event callback for non-blocking)
+    wifi_prov_wait_for_connection(portMAX_DELAY);
+
+    while (true)
+    {
+
+        vTaskDelay(1);
+    }
 }
